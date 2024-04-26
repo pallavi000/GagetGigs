3a8e9506b3d1244f73550e837297a38e19ec13a4
pallavi bhattarai <67225012+pallavi000@users.noreply.github.com>
2024-04-26 18:13:58 +0300

    initial

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+<v-app>
+  <v-main>
+    <v-container>
+      <v-card text="hello world"></v-card>
+    </v-container>
+  </v-main>
+</v-app>
+</template>
+
+<script setup lang="ts">
+  //
+</script>
